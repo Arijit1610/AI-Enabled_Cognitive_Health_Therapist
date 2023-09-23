cd39985951358f3b04a57d7e139151954e46d03d
Arijit Nandy <nandyarijit1610@gmail.com>
2023-09-23 20:19:05 +0530

    commit

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-	return render_template('index.html')
+	return render_template('home.html')
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
 app.run(debug=True) 
